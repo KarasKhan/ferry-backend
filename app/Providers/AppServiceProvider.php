b6fa4fcd0822e2a355eaf1e45830116af9ce30a8
Karas <karasgupta@gmail.com>
2025-11-30 15:05:02 +0800

    Force HTTPS on Request Server Var2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,29 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA IMPORT INI
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         // Deteksi jika di Production (Railway)
         if ($this->app->environment('production')) {
-            // 1. Paksa Generator URL pakai HTTPS
-            \Illuminate\Support\Facades\URL::forceScheme('https');
+            // 1. Paksa URL Generator pakai HTTPS
+            URL::forceScheme('https');
             
-            // 2. Paksa Objek Request menganggap dirinya HTTPS (PENTING BUAT LIVEWIRE)
+            // 2. Paksa Request Object sadar HTTPS (Penting buat Livewire)
             $this->app['request']->server->set('HTTPS', 'on');
         }
     }
