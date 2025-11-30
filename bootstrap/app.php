4cd7d6e52d628fcd7b5da3a7ccafee6dc2391db8
Karas <karasgupta@gmail.com>
2025-11-30 16:50:37 +0800

    Fix TrustProxies Headers for Railway 405 Error3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,17 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
-        // PERBAIKAN: Trust Proxy Lengkap (IP & Headers)
-        $middleware->trustProxies(
-            at: '*',
-            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
-        );
-
+        // Trust All Proxies (Simpel dan Ampuh buat Railway)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
